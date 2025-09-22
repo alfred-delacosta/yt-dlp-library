3df23e5b2662520927ea2e7c7e1f5758190eec43
alfred-delacosta <132242610+alfred-delacosta@users.noreply.github.com>
2025-09-22 18:14:50 -0400

    Adjusted the video cards for a better layout.

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -3,18 +3,17 @@ import { Trash2, MonitorUp, HardDriveDownload, SquareArrowOutUpRight, Pencil } f
 import toast from "react-hot-toast";
 
 const Video = ({ video, serverUrl, deleteVideoButtonClick, setVideoLibrary, api }) => {
-    console.log(video);
   return (
     <div>
         <div className="card shadow">
             {video.thumbnailPath && (
-                <video className="w-100" loading="lazy" preload="meta" controls src={import.meta.env.PROD ? `/${video.videoPath}` : `${serverUrl}/${video.videoPath}`} poster={import.meta.env.PROD ? `/${video.thumbnailPath}` : `${serverUrl}/${video.thumbnailPath}`}></video>
+                <video className="w-100 h-100vh" loading="lazy" preload="meta" controls src={import.meta.env.PROD ? `/${video.videoPath}` : `${serverUrl}/${video.videoPath}`} poster={import.meta.env.PROD ? `/${video.thumbnailPath}` : `${serverUrl}/${video.thumbnailPath}`}></video>
             )}
             {video.thumbnailPath === undefined && (
-                <video className="w-100" loading="lazy" preload="meta" controls src={import.meta.env.PROD ? `/${video.videoPath}` : `${serverUrl}/${video.videoPath}`} poster=""></video>
+                <video className="w-100 h-100vh" loading="lazy" preload="meta" controls src={import.meta.env.PROD ? `/${video.videoPath}` : `${serverUrl}/${video.videoPath}`} poster=""></video>
             )}
             {video.thumbnailPath === null && (
-                <video className="w-100" loading="lazy" preload="meta" controls src={import.meta.env.PROD ? `/${video.videoPath}` : `${serverUrl}/${video.videoPath}`} poster=""></video>
+                <video className="w-100 h-100vh" loading="lazy" preload="meta" controls src={import.meta.env.PROD ? `/${video.videoPath}` : `${serverUrl}/${video.videoPath}`} poster=""></video>
             )}
             <div className="card-body">
                 <h5 className="card-title">{video.name}</h5>
